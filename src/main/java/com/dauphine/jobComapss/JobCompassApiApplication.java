cc8775cae772edf2448a3f46dbba202b922bb756
asma-ben1 <asma.benzine01@gmail.com>
2025-05-17 15:22:23 +0200

    initialze project

---
+++ src/main/java/com/dauphine/jobComapss/JobCompassApiApplication.java
@@ -0,0 +1,13 @@
+package com.dauphine.jobComapss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobCompassApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobCompassApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dauphine/jobComapss/JobCompassApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dauphine.jobComapss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobCompassApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
